c581844e25d10d0e2b6ab1635e41f8fa7108b725
VaDmitrii <dmitrii.vasilevich@gmail.com>
2023-03-01 12:58:26 +0100

    ad category, goals and comments

--- todolist/settings.py
+++ todolist/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
