ddd416aaba72775c97cf4c6391dde50b73b92c13
VaDmitrii <dmitrii.vasilevich@gmail.com>
2023-03-01 13:04:40 +0100

    ad category, goals and comments

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,6 @@ FROM python:3.10-slim
 
 WORKDIR app/
 COPY requirements.txt .
-RUN apt-get update \
-    && apt-get -y install libpq-dev gcc
 RUN pip install -r requirements.txt
-RUN apt-get autoremove -y gcc
 COPY . .
 CMD python manage.py runserver 0.0.0.0:8000
--- todolist/settings.py
+++ todolist/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
